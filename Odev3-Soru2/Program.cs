ce30fa571f0ff5957a6907b48fcabd4a8638bd42
Yusufkr03 <yusufkr2903@gmail.com>
2024-11-04 21:46:21 +0300

    Add files via upload

---
+++ Odev3-Soru2/Program.cs
@@ -0,0 +1,11 @@
+﻿int toplam = 0;
+Console.WriteLine("Bir cümle giriniz");
+string cumle = Console.ReadLine();
+string[] say = cumle.Split(" ");
+foreach (string i in say)
+{
+    Console.WriteLine(i);
+    toplam++;
+}
+Console.WriteLine("Kelime sayısı:"+toplam);
+Console.ReadKey();
